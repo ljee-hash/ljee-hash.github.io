6de29c999cbce3b1cfa15b777e90a54984c6d0c8
caozhilong1 <caozhilong1@jd.com>
2018-02-07 20:46:45 +0800

    支持在线预览流程图

---
+++ _site/js/index.js
@@ -0,0 +1,15 @@
+let path = null
+
+setInterval(() => {
+  if (path !== window.location.pathname) {
+    path = window.location.pathname
+    document.querySelectorAll('.lang-mermaid').forEach(node => {
+      const newNode = node.cloneNode(true)
+      newNode.removeAttribute('class')
+      const hr = document.createElement('hr')
+      node.parentNode.insertBefore(hr, node)
+      node.parentNode.insertBefore(newNode, hr)
+    })
+    window.mermaid.init(undefined, document.querySelectorAll('.lang-mermaid'))
+  }
+}, 1000)
